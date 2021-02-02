b32e37de473859727d9777cb1390b16aa8ce2e29
Tom Bauer <tbauer1898@gmail.com>
2021-02-02 12:42:03 -0600

    extracted all state from node component into grid, wall state of every node is now handled in grid. Made grid updating function to practice immutability.

--- pathfinder/src/Components/Grid/Grid.js
+++ pathfinder/src/Components/Grid/Grid.js
@@ -10,7 +10,21 @@ class Grid extends React.Component {
         this.state = {
             nodes: [],
             mouseDown: false,
-            rightClick: false
+        }
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+        this.handleMouseEnter = this.handleMouseEnter.bind(this);
+        this.handleMouseUp = this.handleMouseUp.bind(this);
+
+    }
+
+    createNode(col, row) {
+        return {
+            wall: false,
+            start_node: false,
+            end_node: false,
+            weight: false,
+            col: col,
+            row: row,
         }
     }
 
@@ -19,35 +33,63 @@ class Grid extends React.Component {
         for (let row = 0; row < 15; row++) {
             const currentRow = [];
             for (let col = 0; col < 40; col++) {
-                currentRow.push(null)
+                currentRow.push(this.createNode(col, row))
             }
             nodes.push(currentRow)
         }
         this.setState({nodes : nodes})
     }
 
-    handleMouseDown() {
-        this.setState({mouseDown: true})
+    updateGrid(nodes, row, col) {
+        let newGrid = nodes.slice() // create copy so that we do not directly mutate state
+        const currentNode = newGrid[row][col];
+        const newNode = {
+            ...currentNode,
+            wall: !currentNode.wall
+        };
+        newGrid[row][col] = newNode;
+        return newGrid;
+    }
+
+    // how can I return the row and column of where the mouse is being pressed?
+
+    handleMouseDown(row, col) {
+        const updatedGrid = this.updateGrid(this.state.nodes, row, col);
+        this.setState({nodes: updatedGrid, mouseDown: true});
     }
 
     handleMouseUp() {
         this.setState({mouseDown: false})
     }
 
-    
+    handleMouseEnter(row, col) {
+        if(!this.state.mouseDown) return;
+        const updatedGrid = this.updateGrid(this.state.nodes, row, col)
+        this.setState({nodes: updatedGrid})
+    }
+
 
     render() {
         const {nodes} = this.state;
         console.log(nodes)
         
         return (
-            <div className='grid' onMouseDown={() => this.handleMouseDown()} onMouseUp={() => this.handleMouseUp()}>
+            <div className='grid' >
                 {nodes.map((row, i) => {
                     return (
                         <div key={i}>
                             {row.map((node, j) => {
                                 return (
-                                    <Node key={i * 40 + j} mouseStatus={this.state.mouseDown} ></Node>
+                                    <Node
+                                        key={i * 40 + j}
+                                        mouseStatus={this.state.mouseDown}
+                                        nodeInfo={node}
+                                        handleMouseDown={this.handleMouseDown}
+                                        handleMouseUp={this.handleMouseUp}
+                                        handleMouseEnter={this.handleMouseEnter}
+                                         >
+
+                                         </Node>
                                 )
                             })}
                         </div>
--- pathfinder/src/Components/Node/Node.js
+++ pathfinder/src/Components/Node/Node.js
@@ -2,32 +2,21 @@ import React from 'react';
 import './Node.css'
 
 class Node extends React.Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-            wall: false,
-            
-        }
-    }
-
-    buildAndBreakWall() {
-        if(!this.state.wall) {
-            this.setState({wall: true});
-            return;
-        }
-        this.setState({wall: false})
-        
-    }
+    
+    
 
     render() {
+        const { nodeInfo, mouseStatus, handleMouseDown, handleMouseUp, handleMouseEnter } = this.props
+
     return (
         <div 
-        className={`${this.state.wall ? 'wall' : ''} node`}
-        onMouseEnter={this.props.mouseStatus ? () => this.buildAndBreakWall() : null}
-        onMouseDown={() => this.buildAndBreakWall()}
+        className={`${nodeInfo.wall ? 'wall' : ''} node`}
+        
+        onMouseEnter={() => handleMouseEnter(nodeInfo.row, nodeInfo.col)}
+        onMouseDown={() => handleMouseDown(nodeInfo.row, nodeInfo.col)}
+        onMouseUp={() => handleMouseUp()}
+        
         >
-            {this.props.value}
         </div>
     )
     }
